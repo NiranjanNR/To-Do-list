d2f35fe99ef00ee0024d7de1b43a0b86e45cf3d6
NiranjanNR <84635960+NiranjanNR@users.noreply.github.com>
2022-07-25 17:53:04 +0530

    Final changes in UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react';
 import { db } from './firebase-config';
 import { collection,getDocs,addDoc,deleteDoc,doc } from 'firebase/firestore';
+import './index.css';
 var i = 2;
 function App() {
 
@@ -64,17 +65,22 @@ function App() {
   }
 
   return (
-    <div className="text-3xl text-center p-16 bg-zinc-900 text-white ">
-      <h1 className="text-8xl">To-Do List</h1><br />
-      <div className="lg:mx-36 ">
+    <div className="text-3xl text-center md:p-12 ">
+    <h1 className="md:text-8xl text-7xl	text-neutral-800 ">To-Do List</h1><br />
+    <div className="md:flex md:mx-10 md:my-12 mx-10 hmm">
+      <div>
+        <h1 className="text-neutral-900 md:text-left text-5xl md:mt-20 lg:text-7xl mb-7 my-3">Write the tasks<br /> you have left to complete</h1>
+      </div>
+      <div className="blur-me rounded-3xl p-10 lg:mx-20 ">
       {count.map((iterate,index) => 
         <div className="flex">{true ? <div className="p-2">{index+1}.</div> :<></>}
         <div className="ml-2 p-2">{iterate.Task}</div>
-        {true ? <button className="ml-auto text-xl text-white bg-stone-500 rounded-lg hover:bg-stone-700 p-1 px-3 my-2" onClick={() => del(index,iterate.id)}>Delete</button> :<></>}
+        {true ? <button className="ml-auto text-xl text-white rounded-lg bg-neutral-700/50 hover:bg-neutral-700/75 p-1 px-3 my-2" onClick={() => del(index,iterate.id)}>Delete</button> :<></>}
       </div>)}
+      <input  type="text" className="text-center text-xl sm:text-2xl text-black/75 border-2 border-black placeholder:text-black/75 bg-transparent rounded-lg my-4 py-1 md:px-6 md:py-2" value={value} onChange={e => setValue(e.target.value)} onKeyDown={Change} placeholder="Add a task" /><br />
+      <button className="text-xl text-white/75 bg-neutral-700/50 hover:bg-neutral-700/75 rounded-lg p-1 px-4 " onClick={Click} >Add</button>
       </div>
-      <input  type="text" className="text-center text-xl sm:text-3xl text-white bg-stone-500 rounded-lg hover:bg-stone-700 my-4 md:px-4 md:py-1" value={value} onChange={e => setValue(e.target.value)} onKeyDown={Change} placeholder="Add a task" /><br />
-      <button className="text-xl text-white bg-stone-500 rounded-lg hover:bg-stone-700 p-1 px-3 " onClick={Click} >Add</button>
+    </div>
     </div>
   );
 }
